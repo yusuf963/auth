15e571e6d56cb3b101e38ce859aa830123af941b
yusuf963 <mohammad963yusuf@gmail.com>
2021-02-21 23:59:37 +0000

    adding

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+import express from 'express';
+import bodyParser from 'body-parser'
+
+const app = express()
+
+app.use(express.static('public'))
+
+
+
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+  console.log('🚀 server up and running ')
+})
+
 
